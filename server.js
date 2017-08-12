6e499799fda15e9ee0094c68f90b1d27dbd140fe
mukeshsolankii <30656280+mukeshsolankii@users.noreply.github.com>
2017-08-12 17:29:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
  
 app.get('/article-one', function (req, res){
-    res.sendFile(path.join(__dirname,ui,'article-one.html'));
+    res.sendFile(path.join(__dirname,'ui', 'article-one.html'));
 }); 
 
 app.get('/article-two', function(req, res){
