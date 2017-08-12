f9212c96e36772ee9599379edcc854c0ae3fef5b
mukeshsolankii <30656280+mukeshsolankii@users.noreply.github.com>
2017-08-12 19:08:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,14 +16,21 @@ var img  = document.getElementById('madi');
      }
  }
  
- 
- 
  function move(){
-  
           setInterval(moveRight, 50);
-     
-     
  }
+ 
+ //*************************************************//
+ var counter = 0;
+  
+ var button = document.getElemetById('counter');
+  button.onclick = function(){
+      counter = counter+ 1;
+      var span = document.getElementById('count');
+      span.innerHTML = counter;
+  };
+  
+ 
  
 
  
