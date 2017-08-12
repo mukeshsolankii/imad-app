a4c842440c613cbd2300c040b83db8d02bb082da
mukeshsolankii <30656280+mukeshsolankii@users.noreply.github.com>
2017-08-12 18:10:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,11 @@
 console.log('Loaded!');
 console.log('musa | power');
+
+
+var img  = document.getElementById('madi');
+ element.onclick = ('move');
+ 
+ 
+ function move(){
+     element.style.marginLeft = '50px';
+ }
