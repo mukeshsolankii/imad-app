0c628cf3fc78fc1a1c9eac7687c8a6cd1e160f6b
mukeshsolankii <30656280+mukeshsolankii@users.noreply.github.com>
2017-08-12 18:59:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,29 +31,35 @@ return  templete;
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/ui/main.js',function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
- 
-app.get('/article-one', function (req, res){
-    res.send(createTemplete(articleOne));
-}); 
-
-app.get('/article-two', function(req, res){
-    res.send('<h2>Here article-two is sencirely presents to you.</h2>');
-});
+            app.get('/', function (req, res) {
+              res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+            });
+            
+                app.get('/ui/main.js',function (req, res){
+                    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+                });
+                
+                    app.get('/ui/style.css', function (req, res) {
+                      res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+                    });
+                    var counter = 0;
+                    app.get('/counter', function(req, res){
+                        counter = counter + 1;
+                        res.send(counter.toString());
+                    });
+                    
+                    
+                    app.get('/ui/madi.png', function (req, res) {
+                      res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+                    });
+                 
+                app.get('/article-one', function (req, res){
+                    res.send(createTemplete(articleOne));
+                }); 
+            
+            app.get('/article-two', function(req, res){
+                res.send('<h2>Here article-two is sencirely presents to you.</h2>');
+            });
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
