e565aa87d926bdc7428e611d5bafd491f49e0a9b
mukeshsolankii <30656280+mukeshsolankii@users.noreply.github.com>
2017-09-24 00:07:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -64,9 +64,11 @@ function login(){
             }
         }
         //not yet
-    }
+    };
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
     //make request..
     request.open('POST','http://musasurvey1616.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type' , 'application/json');
